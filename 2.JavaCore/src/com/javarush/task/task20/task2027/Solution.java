9779450c15fdffa0d890bcbb55d252ecabac621e
policorp <policorp22@yandex.ru>
2018-09-13 17:47:13 +0400

    Update

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task20.task2027;
 
+import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.util.List;
 
 /* 
@@ -20,11 +22,90 @@ public class Solution {
 home - (5, 3) - (2, 0)
 same - (1, 1) - (4, 1)
          */
+
+        for (Word word: detectAllWords(crossword, "home", "same")) {
+            System.out.println(word.toString());
+        }
+        System.out.println("end");
     }
 
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        List<Word> list = new ArrayList<>();
+        Word word = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        Integer height = crossword.length; //stroka - x
+        Integer width = crossword[0].length; //stlobec - y
 
-        return null;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                //current start position crossword[i][j]
+
+                //j++
+                for (int k = j; k < width; k++) {
+                    /*
+                    reshit' problemu preobrazovaniya int to char
+                    inache on vmesto bukvi dobavlyaet kod simvola v stringbuilder
+
+                     */
+                    String ss = Character.getName(crossword[i][k]);
+                    System.out.println(ss);
+                    stringBuilder.append(crossword[i][k]);
+                    for (String s: words) {
+                        if (s.equals(stringBuilder.toString())) {
+                            word = new Word(s);
+                            word.setStartPoint(i, j);
+                            word.setEndPoint(i, k);
+                            list.add(word);
+                        }
+                    }
+                }
+
+                //j--
+                stringBuilder = new StringBuilder();
+                for (int k = j; k >= 0; k--) {
+                    stringBuilder.append(crossword[i][k]);
+                    for (String s: words) {
+                        if (s.equals(stringBuilder.toString())) {
+                            word = new Word(s);
+                            word.setStartPoint(i, j);
+                            word.setEndPoint(i, k);
+                            list.add(word);
+                        }
+                    }
+                }
+
+                //i++
+                stringBuilder = new StringBuilder();
+                for (int m = i; m < height; m++) {
+                    stringBuilder.append(crossword[m][j]);
+                    for (String s: words) {
+                        if (s.equals(stringBuilder.toString())) {
+                            word = new Word(s);
+                            word.setStartPoint(i, j);
+                            word.setEndPoint(m, j);
+                            list.add(word);
+                        }
+                    }
+                }
+
+                //i--
+                stringBuilder = new StringBuilder();
+                for (int m = i; m >= 0; m--) {
+                    stringBuilder.append(crossword[m][j]);
+                    for (String s: words) {
+                        if (s.equals(stringBuilder.toString())) {
+                            word = new Word(s);
+                            word.setStartPoint(i, j);
+                            word.setEndPoint(m, j);
+                            list.add(word);
+                        }
+                    }
+                }
+
+
+            }
+        }
+        return list;
     }
 
     public static class Word {
