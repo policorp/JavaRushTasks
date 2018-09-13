01b6522bfda93b6635f5c78160751cf79330cdb6
policorp <policorp22@yandex.ru>
2018-09-13 15:34:41 +0400

    Update

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -25,6 +25,41 @@ public class Solution {
     }
 
     public static int getRectangleCount(byte[][] a) {
-        return 0;
+        int size = a.length;
+        int rectCounter = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                //search for 1 only
+                if ((a[i][j] == -1) || (a[i][j] == 0))
+                    continue;
+
+                //start point at i, j
+                int width = 0;
+                for (int k = i; k < size; k++) {
+                    if (a[k][j] == 0)
+                        break;
+                    width++;
+                }
+
+                int height = 0;
+                for (int m = j; m < size; m++) {
+                    if (a[i][m] == 0)
+                        break;
+                    height++;
+                }
+
+//                System.out.println("i= " + i + ", j=" + j);
+//                System.out.println(width + " " + height);
+
+                for (int g = i; g < width + i; g++) {
+                    for (int f = j; f < height + j; f++) {
+                        a[g][f] = -1;
+                    }
+                }
+
+                rectCounter++;
+            }
+        }
+        return rectCounter;
     }
 }
