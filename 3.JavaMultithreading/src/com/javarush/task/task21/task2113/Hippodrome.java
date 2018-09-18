69239be5ecb7c190bc9b43ab85b0199d0dd849ab
policorp <policorp22@yandex.ru>
2018-09-18 13:56:38 +0400

    Update - Hippodrome - 8

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -30,7 +30,12 @@ public class Hippodrome {
         }
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            this.move();
+            this.print();
+            Thread.sleep(200);
+        }
     }
 
     public void move() {
