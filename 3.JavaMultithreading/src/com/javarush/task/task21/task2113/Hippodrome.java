bbcd16b59d527880ab9dcfb3350801fd0ddb4260
policorp <policorp22@yandex.ru>
2018-09-18 14:01:12 +0400

    Update - Hippodrome - 10

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -45,6 +45,12 @@ public class Hippodrome {
     }
 
     public void print() {
+        for (Horse horse: this.getHorses()) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
     }
 
     public List<Horse> getHorses() {
