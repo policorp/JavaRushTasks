b6e6fc4174b6f8a08f0beb44c8ee09fc0726ffe0
policorp <policorp22@yandex.ru>
2018-09-18 13:52:28 +0400

    Update

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,12 +1,36 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by m.polyakov on 18.09.2018.
  */
 
 
 public class Hippodrome {
+    private List<Horse> horses;
+    public static Hippodrome game;
     public static void main(String[] args) {
         System.out.println("Hello hippodrome!");
+        List<Horse> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Horse horse = new Horse("name" + i, 3, 0);
+            list.add(horse);
+        }
+        game = new Hippodrome(list);
+    }
+
+    public Hippodrome(List horses) {
+        if (horses != null) {
+            this.horses = horses;
+        }
+        else {
+            this.horses = new ArrayList<>();
+        }
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -4,4 +4,37 @@ package com.javarush.task.task21.task2113;
  * Created by m.polyakov on 18.09.2018.
  */
 public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
