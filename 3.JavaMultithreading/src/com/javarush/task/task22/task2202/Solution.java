8a8f7624f0a673b48cfacc8fce9a1a3d039692cb
policorp <policorp22@yandex.ru>
2018-09-20 15:42:26 +0400

    Update

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -1,29 +1,31 @@
 package com.javarush.task.task22.task2202;
 
-/* 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
 Найти подстроку
 */
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+//        System.out.println(getPartOfString("Амиго и Диего лучшие друзья!"));
     }
 
     public static String getPartOfString(String string) {
-        String answer = string;
-        int start = 0;
-        int end = string.length();
-        try {
-            //
-            start = string.indexOf(" ") + 1;
-            for (int i = 0; i < 4; i++) {
-
-            }
-            answer = answer.substring(start);
-        }
-        catch (Exception e) {
+        if (string == null)
             throw new TooShortStringException();
+        string += ' ';
+        int fisrtSpace = string.indexOf(' ');
+        if (fisrtSpace == -1)
+            throw new TooShortStringException();
+        int fourthSpace = fisrtSpace;
+        for (int i = 0; i < 4; i++) {
+            fourthSpace = string.indexOf(' ', fourthSpace + 1);
+            if (fourthSpace == -1)
+                throw new TooShortStringException();
         }
-        return answer;
+        return string.substring(fisrtSpace + 1, fourthSpace);
     }
 
     public static class TooShortStringException extends RuntimeException {
